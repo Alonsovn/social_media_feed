9f3a07b3911cebd373f7a67e1e569c03c59355db
Alonsovn <alonsonh94@gmail.com>
2022-08-22 15:01:34 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,29 @@
-print("Hello! I'm a template project")
+from fastapi import FastAPI, Request, Response, Depends, staticfiles
+from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# Pages
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Friends Connect - Home"})
+
+
+@app.get("/login", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request, "title": "Friends Connect - Login"})
+
+
+# API
+@app.post("/login", response_class=RedirectResponse)
+def login(request: Request, response: Response,
+          form_data: OAuth2PasswordRequestForm = Depends()):
+
+
